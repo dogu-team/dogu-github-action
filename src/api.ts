4aa66040b6394107d54cc6b4b3dc451b38306c9e
Seong JuWon <lumyjuwon@gmail.com>
2023-08-20 00:03:22 +0900

    refactor: move websocket to api

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,8 @@
 import axios from "axios";
+import WebSocket from "ws";
+import * as core from '@actions/core';
+
+import { PipelieState } from './types';
 
 export module API {
   export type RunRoutine = {
@@ -11,6 +15,22 @@ export module API {
     createdAt: Date;
   }
 
+  function getSocketUrl(apiUrl: string) {
+    const apiUrlObj = new URL(apiUrl);
+    let socketUrl = '';
+  
+    if (apiUrlObj.protocol === 'http:') {
+      socketUrl = apiUrlObj.port === '' ? `ws://${apiUrlObj.hostname}` : `ws://${apiUrlObj.hostname}:${apiUrlObj.port}`;
+    }
+    else if (apiUrlObj.protocol === 'https:') {
+      socketUrl = apiUrlObj.port === '' ? `wss://${apiUrlObj.hostname}` : `wss://${apiUrlObj.hostname}:${apiUrlObj.port}`;
+    }
+    else {
+      core.setFailed(`Unsupported protocol: ${apiUrlObj.protocol}`);
+      process.exit(1);
+    }
+  }
+
   export async function runRoutine(apiUrl: string, projectId: string, routineId: string): Promise<RunRoutine> {
     const result = await axios.post<RunRoutine>(`${apiUrl}/v1/projects/${projectId}/routines/${routineId}/pipelines`, undefined, {
       headers: {
@@ -19,5 +39,46 @@ export module API {
     })
 
     return result.data;
+  }
+
+  export async function connectRoutine(apiUrl: string, projectId: string, routineId: string, routine: RunRoutine) {
+    const socketUrl = getSocketUrl(apiUrl);
+
+    const client = new WebSocket(`${socketUrl}/v1/pipeline-state?projectId=${projectId}&routineId=${routineId}&pipelineId=${routine.routinePipelineId}`, {
+      headers: {
+        'Authorization': `Bearer ${process.env.DOGU_TOKEN}`
+      }
+    });
+  
+    client.on('message', (state) => {
+      const pipelineState: PipelieState = JSON.parse(state.toString());
+  
+      switch (pipelineState.state) {
+        case 'SUCCESS':
+          console.log(`Routine succeeded. Look at the result: ${pipelineState.resultUrl}`);
+          process.exit(0);
+        case 'FAILURE':
+        case 'CANCELLED':
+        case 'SKIPPED':
+          console.log(`Routine ${pipelineState.state.toLowerCase()}. Look at the result: ${pipelineState.resultUrl}`);
+          process.exit(1);
+      }
+    })
+  
+    client.on('close', (code, reason) => {
+      if (code === 1000) {
+        process.exit(0);
+      }
+      else {
+        core.setFailed(reason.toString());
+        process.exit(1);
+      }
+    });
+  
+    client.on('error', (error) => {
+      console.error(error);
+      core.setFailed(error.toString());
+      process.exit(1);
+    });
   }
 }
--- src/routine.ts
+++ src/routine.ts
@@ -1,9 +1,7 @@
 import './types'
 import * as core from '@actions/core';
-import WebSocket from "ws";
 
 import { API } from './api';
-import { PipelieState } from './types';
 
 
 export async function runRoutine(apiUrl: string, projectId: string, routineId: string) {
@@ -23,54 +21,5 @@ export async function runRoutine(apiUrl: string, projectId: string, routineId: s
     process.exit(1);
   }
 
-  const apiUrlObj = new URL(apiUrl);
-  let socketUrl = '';
-
-  if (apiUrlObj.protocol === 'http:') {
-    socketUrl = apiUrlObj.port === '' ? `ws://${apiUrlObj.hostname}` : `ws://${apiUrlObj.hostname}:${apiUrlObj.port}`;
-  }
-  else if (apiUrlObj.protocol === 'https:') {
-    socketUrl = apiUrlObj.port === '' ? `wss://${apiUrlObj.hostname}` : `wss://${apiUrlObj.hostname}:${apiUrlObj.port}`;
-  }
-  else {
-    core.setFailed(`Unsupported protocol: ${apiUrlObj.protocol}`);
-    process.exit(1);
-  }
-
-  const client = new WebSocket(`${socketUrl}/v1/pipeline-state?projectId=${projectId}&routineId=${routineId}&pipelineId=${routine.routinePipelineId}`, {
-    headers: {
-      'Authorization': `Bearer ${process.env.DOGU_TOKEN}`
-    }
-  });
-
-  client.on('message', (state) => {
-    const pipelineState: PipelieState = JSON.parse(state.toString());
-
-    switch (pipelineState.state) {
-      case 'SUCCESS':
-        console.log(`Routine succeeded. Look at the result: ${pipelineState.resultUrl}`);
-        process.exit(0);
-      case 'FAILURE':
-      case 'CANCELLED':
-      case 'SKIPPED':
-        console.log(`Routine ${pipelineState.state.toLowerCase()}. Look at the result: ${pipelineState.resultUrl}`);
-        process.exit(1);
-    }
-  })
-
-  client.on('close', (code, reason) => {
-    if (code === 1000) {
-      process.exit(0);
-    }
-    else {
-      core.setFailed(reason.toString());
-      process.exit(1);
-    }
-  });
-
-  client.on('error', (error) => {
-    console.error(error);
-    core.setFailed(error.toString());
-    process.exit(1);
-  });
+  API.connectRoutine(apiUrl, projectId, routineId, routine);
 }
